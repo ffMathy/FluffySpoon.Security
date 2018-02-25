60b5a92249fe17be61adc964d87e1dc83cc03509
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-02-25 19:05:11 +0100

    using argon2.

---
+++ src/FluffySpoon.Security.Hashing/Hasher.cs
@@ -0,0 +1,16 @@
+﻿using Isopoh.Cryptography.Argon2;
+using System;
+
+namespace FluffySpoon.Security.Hashing
+{
+    public class Hasher
+    {
+		public string GenerateHash(string password) {
+			return Argon2.Hash(password);
+		}
+
+		public bool VerifyHash(string hash, string value) {
+			return Argon2.Verify(hash, value);
+		}
+    }
+}
