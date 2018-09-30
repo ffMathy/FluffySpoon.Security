907bbac91c471066fa8742d90e423a1f4af258d4
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-09-30 03:49:32 +0200

    null checks.

--- src/FluffySpoon.Security.Hashing/Hasher.cs
+++ src/FluffySpoon.Security.Hashing/Hasher.cs
@@ -1,14 +1,21 @@
 ﻿using Isopoh.Cryptography.Argon2;
+using System;
 
 namespace FluffySpoon.Security.Hashing
 {
     public class Hasher : IHasher
 	{
 		public string Generate(string password) {
+			if(password == null)
+				throw new ArgumentNullException(nameof(password));
+
 			return Argon2.Hash(password);
 		}
 
 		public bool Verify(string hash, string value) {
+			if(password == null)
+				return false;
+
 			return Argon2.Verify(hash, value);
 		}
     }
