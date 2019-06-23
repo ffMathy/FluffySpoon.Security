f72c4b7805e81f43f348fc5ea0cc4c2f761b1926
ffMathy <mathias.lorenzen@live.com>
2019-06-23 12:58:39 +0200

    adjustable parameters.

--- src/FluffySpoon.Security.Hashing/Strategies/Argon2VersionStrategy.cs
+++ src/FluffySpoon.Security.Hashing/Strategies/Argon2VersionStrategy.cs
@@ -2,18 +2,44 @@
 
 namespace FluffySpoon.Security.Hashing.Strategies
 {
-	class Argon2VersionStrategy : ILatestVersionStrategy, IVersionStrategy
-	{
-		public string Prefix => "argon2";
+    class Argon2VersionStrategy : ILatestVersionStrategy, IVersionStrategy
+    {
+        private readonly IArgon2Settings settings;
 
-		public string Generate(string password)
-		{
-			return Argon2.Hash(password);
-		}
+        public string Prefix => "argon2";
 
-		public bool Verify(string hash, string value)
-		{
-			return Argon2.Verify(hash, value);
-		}
-	}
+        public Argon2VersionStrategy(IArgon2Settings settings)
+        {
+            this.settings = settings;
+        }
+
+        public string Generate(string password)
+        {
+            return Argon2.Hash(password, 
+                settings.TimeCost ?? 3, 
+                settings.MemoryCost ?? 65536, 
+                settings.Parallelism ?? 1);
+        }
+
+        public bool Verify(string hash, string value)
+        {
+            return Argon2.Verify(hash, value);
+        }
+    }
+
+    public interface IArgon2Settings
+    {
+        int? TimeCost { get; }
+        int? Parallelism { get; }
+        int? MemoryCost { get; }
+    }
+
+    public class Argon2Settings : IArgon2Settings
+    {
+        public int? TimeCost { get;set; }
+
+        public int? Parallelism { get; set; }
+
+        public int? MemoryCost { get; set; }
+    }
 }
